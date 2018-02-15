f6cdd05d27055e4655cf47a0f64e0660a674a5c9
raguila8 <rodrigoaguilar887@gmail.com>
2018-02-15 04:24:25 -0800

    aws config

--- config/initializers/s3.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => "AKIAJBNEOEY6M27OWFJA",
-      :aws_secret_access_key  => "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",
-      :region                 => 'us-west-1' # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = "petgram"
-end
